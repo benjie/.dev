7ea6a5a64af916843357e28e6155ea6774ef4dc7
Benjie Gillam <benjie@jemjie.com>
2023-10-16 17:10:10 +0100

    Styling of calendly embeds

--- components/Calendly.tsx
+++ components/Calendly.tsx
@@ -1,9 +1,25 @@
-import React, { FC, useEffect, useRef } from "react";
+import { useConfig } from "nextra-theme-docs";
+import React, { FC, useEffect, useLayoutEffect, useRef, useState } from "react";
 export const Calendly: FC<{ url?: string }> = ({
-  url = "https://calendly.com/graphile/one_on_one",
+  url: baseUrl = "https://calendly.com/graphile/one_on_one",
 }) => {
   const ref = useRef(null);
+  const config = useConfig();
+  const [isDarkMode, setIsDarkMode] = useState(undefined);
+  useLayoutEffect(() => {
+    setIsDarkMode(
+      getComputedStyle(document.documentElement).colorScheme === "dark",
+    );
+  }, []);
+  const url =
+    baseUrl +
+    `?hide_event_type_details=1` +
+    (isDarkMode
+      ? `&background_color=111111&text_color=ffffff&primary_color=008ae6`
+      : `&background_color=ffffff&text_color=000000&primary_color=008ae6`);
+
   useEffect(() => {
+    ref.current.innerHTML = "";
     (window.Calendly as any)?.initInlineWidget({
       url,
       parentElement: ref.current,
@@ -11,14 +27,19 @@ export const Calendly: FC<{ url?: string }> = ({
       utm: {},
     });
   }, [url]);
+  // ?hide_event_type_details=1&hide_gdpr_banner=1&background_color=000000&text_color=ffffff&primary_color=ff00d5
   return (
     <>
       <div
         ref={ref}
-        className="calendly-inline-widget"
-        // data-url={url}
-        style={{ minWidth: 320, height: 1200 }}
-      ></div>
+        style={{
+          minWidth: 320,
+          maxWidth: 649,
+          minHeight: 600,
+          height: 600,
+          margin: "1rem auto",
+        }}
+      />
     </>
   );
 };
